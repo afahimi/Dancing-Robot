3ff3e989ff62e864136765f201a42fc55c184c04
Amraj-Aujla <amrajaujla@hotmail.com>
2023-02-01 12:03:50 -0800

    Me and Navraj worked on this

--- code.py
+++ code.py
@@ -40,6 +40,76 @@ def dance_move2():
     set_servo_angle(servo_right_thigh, 90)
     set_servo_angle(servo_left_thigh, 90)
     time.sleep(0.5)
+    def dance_move3():
+    i =0
+    while (i<10):
+        print("hi")
+        set_servo_angle(servo_right_foot, 85)
+        set_servo_angle(servo_left_foot, 75)
+        set_servo_angle(servo_right_thigh, 90)
+        set_servo_angle(servo_left_thigh, 90)
+        time.sleep(0.4)
+        set_servo_angle(servo_right_foot, 30)
+        set_servo_angle(servo_left_foot,150 )
+        set_servo_angle(servo_right_thigh, 90)
+        set_servo_angle(servo_left_thigh, 90)
+        time.sleep(0.4)
+        i +=1
+
+        def dance_move3():
+    i =0
+    while (i<10):
+        print("hi")
+        set_servo_angle(servo_right_foot, 85)
+        set_servo_angle(servo_left_foot, 75)
+        set_servo_angle(servo_right_thigh, 90)
+        set_servo_angle(servo_left_thigh, 90)
+        time.sleep(0.4)
+        set_servo_angle(servo_right_foot, 30)
+        set_servo_angle(servo_left_foot,150 )
+        set_servo_angle(servo_right_thigh, 90)
+        set_servo_angle(servo_left_thigh, 90)
+        time.sleep(0.4)
+        i +=1
+
+def dance_move4():
+    
+    i =0
+    while (i<3):
+        print("hi")
+        set_servo_angle(servo_right_foot, 37)
+        set_servo_angle(servo_left_foot, 37)
+        set_servo_angle(servo_right_thigh, 57)
+        set_servo_angle(servo_left_thigh, 57)
+        time.sleep(0.4)
+        set_servo_angle(servo_right_foot, 87)
+        set_servo_angle(servo_left_foot,77)
+        set_servo_angle(servo_right_thigh, 97)
+        set_servo_angle(servo_left_thigh, 97)
+        time.sleep(0.4)
+        i+=1
+        
+    time.sleep(0.4)
+    
+    i = 0
+    while (i<3):
+        print ("BYE")
+        set_servo_angle(servo_right_foot, 90)
+        set_servo_angle(servo_left_foot, 90)
+        set_servo_angle(servo_right_thigh, 60)
+        set_servo_angle(servo_left_thigh, 60)
+        time.sleep(0.6)
+        set_servo_angle(servo_right_foot, 90)
+        set_servo_angle(servo_left_foot,90)
+        set_servo_angle(servo_right_thigh, 140)
+        set_servo_angle(servo_left_thigh, 140)
+        time.sleep(0.6)
+        i +=1
+    set_servo_angle(servo_right_foot, 90)
+    set_servo_angle(servo_left_foot,90)
+    set_servo_angle(servo_right_thigh, 90)
+    set_servo_angle(servo_left_thigh, 90)
+    time.sleep(0.7)  
 
 
 
